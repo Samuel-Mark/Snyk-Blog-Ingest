2e46b57305fcf9622d54d4d0a124877bac6021d2
Samuel-Mark <samuel.mark@sky.uk>
2025-03-13 16:12:55 +0000

    Update JSON Output to be based on Year and Month

--- main.py
+++ main.py
@@ -24,10 +24,16 @@ def main():
         posts = extract_posts(html_content)
         formatted_posts = filter_and_format(posts)
         
-        with open('posts.json', 'w') as json_file:
-            json.dump(formatted_posts, json_file, indent=4)
+        posts_by_date = {}
+        for post in formatted_posts:
+            year_month = f"{post['year']}_{post['month']:02d}"
+            if year_month not in posts_by_date:
+                posts_by_date[year_month] = []
+            posts_by_date[year_month].append(post)
         
-        print("Data has been exported to posts.json")
+        for year_month, posts in posts_by_date.items():
+            with open(f'snky_updates_{year_month}.json', 'w', encoding='utf-8') as json_file:
+                json.dump(posts, json_file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
