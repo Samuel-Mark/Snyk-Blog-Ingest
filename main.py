7fa4fc97ecc3a4fd39f741ba278adf1af8881fbf
Samuel-Mark <samuel.mark@sky.uk>
2025-03-13 17:01:59 +0000

    Add Old to New Entries and Add Sorting of New Entries in Between

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from static import fetch_static
 from dynamic import fetch_dynamic
 from process import extract_posts, filter_and_format
+from pathlib import Path
 
 def main():
     mode = 'static'
@@ -31,13 +32,33 @@ def main():
                 posts_by_date[year_month] = []
             posts_by_date[year_month].append(post)
         
-        for year_month, posts in posts_by_date.items():
-            # with open(f'snky_updates_{year_month}.json', 'w', encoding='utf-8') as json_file:
-            # below works as long as snyk_updates/ directory exists
-            # did not want to import os due to cyber concerns?
-            # A README is included to force the directories existance.
-            with open(f'{'snyk_updates'}/snyk_{year_month}.json', 'w', encoding='utf-8') as json_file:
-                json.dump(posts, json_file, indent=4, ensure_ascii=False)
+        output_dir = Path('snyk_updates')
+        output_dir.mkdir(parents=True, exist_ok=True)
+        
+        for year_month, new_posts in posts_by_date.items():
+            json_file_path = output_dir / f'snyk_{year_month}.json'
+            
+            if json_file_path.exists():
+                with open(json_file_path, 'r', encoding='utf-8') as json_file:
+                    existing_posts = json.load(json_file)
+            else:
+                existing_posts = []
+            
+            # key-value pairs of existing posts
+            existing_posts_dict = {post['title']: post for post in existing_posts}
+            
+            # sort by year, month, day, and time in descending order
+            new_posts.sort(key=lambda x: (x['year'], x['month'], x['day'], x['time']), reverse=True)
+            
+            # update existing or add new
+            for new_post in new_posts:
+                if new_post['title'] not in existing_posts_dict:
+                    existing_posts_dict[new_post['title']] = new_post
+            
+            updated_posts = sorted(existing_posts_dict.values(), key=lambda x: (x['year'], x['month'], x['day'], x['time']))
+
+            with open(json_file_path, 'w', encoding='utf-8') as json_file:
+                json.dump(updated_posts, json_file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
